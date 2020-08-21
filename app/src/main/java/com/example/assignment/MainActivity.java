030eb2cb3dd90cedd5e3c8ea6afd22fc7ed7cdb7
RyanYee <yeezs-wm19@student.tarc.edu.my>
2020-08-21 23:16:18 +0800

    By Yee Zhi Sheng

--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -2,17 +2,11 @@ package com.example.assignment;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
-import com.google.android.material.navigation.NavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
